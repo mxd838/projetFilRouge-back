926c6562e3cc53e1be04395c242e3e0f5f28b15d
mickael j <joseph.mickk@gmail.com>
2022-09-25 17:35:26 +0200

    schema product

---
+++ src/products/schemas/product.schemas.ts
@@ -0,0 +1,81 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+export type ProductDocument = Product & Document;
+
+@Schema()
+export class Product {
+
+  @Prop()
+  name: string;
+
+  @Prop()
+  category: string;
+
+  @Prop()
+  deadline: string;
+
+  @Prop()
+  description: string;
+
+  @Prop()
+  buyers: number;
+
+  @Prop()
+  levels: [
+    {
+      level: number;
+      buyersNeeded: number;
+      price: number;
+      current: boolean;
+    },
+    {
+      level: number;
+      buyersNeeded: number;
+      price: number;
+      current: boolean;
+    },
+    {
+      level: number;
+      buyersNeeded: number;
+      price: number;
+      current: boolean;
+    },
+    {
+      level: number;
+      buyersNeeded: number;
+      price: number;
+      current: boolean;
+    }
+  ]
+
+  @Prop()
+  details: string;
+
+  @Prop()
+  keywords: string;
+
+  @Prop()
+  origin: string;
+
+  @Prop()
+  images: [
+    image: string,
+    image: string,
+    image: string
+  ]
+
+  @Prop()
+  produced: boolean;
+
+  @Prop()
+  craft: boolean;
+
+  @Prop()
+  recycled: boolean;
+
+  @Prop()
+  responsible: boolean;
+
+}
+
+export const ProductDocument = SchemaFactory.createForClass(Product);
